42d555445fe9fc31018aff78f4ee0b7cfa9ca80b
bkwilcox100 <bkwilcox100@gmail.com>
2017-08-04 16:01:14 -0500

    Added ignore file back

--- skelly.js
+++ skelly.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const _ = require('underscore');
+const path = require('path');
 const createTempMap = require('./Functions/createTemplateMap.js').create;
 
 // Get Command Line Arguments
@@ -11,15 +12,30 @@ console.log(argv);
 
 var templates = createTempMap();
 
-if (_.contains(argv['_'], 'help')){
-  fs.readFile('./Docs/helpme.txt', 'utf8', function(err, data){
-    if (err){
+if (_.contains(argv['_'], 'help')) {
+  fs.readFile('./Docs/helpme.txt', 'utf8', function(err, data) {
+    if (err) {
       throw (err);
     }
     console.log(data);
   });
-} else if (templates.has(argv['_'][0])){
-  console.log(templates.get(argv['_'][0]));
+} else if (templates.has(argv['_'][0]) && argv['_'][1]) {
+  var value = templates.get(argv['_'][0]);
+  var fileName = path.join('.', 'Templates', templates.get(argv['_'][0]));
+  fs.readFile(fileName, 'utf8', function(err, data) {
+    if (err) {
+      throw (err);
+    }
+
+    var extension = value.slice(value.indexOf('.'), value.length);
+
+    fs.writeFile(path.join(__dirname, argv['_'][1] + extension), data, function(err) {
+      if (err) {
+        throw (err);
+      }
+      console.log("Success");
+    });
+  });
 } else {
   console.log("Unknown Command. run 'skelly help' for help");
 }
